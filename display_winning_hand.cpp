0f44b3f108ccd663a9ddd730301f62c577a2cacf
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-16 23:21:49 +0530

    Update display_winning_hand.cpp

--- display_winning_hand.cpp
+++ display_winning_hand.cpp
@@ -6,6 +6,41 @@
 #include<cstdlib> 
 using namespace std;
 
+vector<string> testv =
+{
+	"qc",
+	"jc",
+	"ac",
+	"3d",
+	"kc",
+	"tc",
+};
+
+vector<string> straight = 
+
+{
+	"a",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"t",
+	"j",
+	"q",
+	"k",
+	"a",
+	"2",
+	"3",
+	"4",
+	"5",
+}; 
+	
+
+
 class poker	
 {
 	public: 
@@ -19,6 +54,7 @@ class poker
 	std::vector<int> dealt_cards;
 	std::vector<string> hand; 
 	std::vector<string> table;
+	std::vector<char> tstraight;
 	
 	poker();
 	void play(); 
@@ -39,9 +75,401 @@ class poker
 	bool toak_(vector<string> match); 
 	bool twopair_(vector<string> match);
 	bool pair_(vector<string> match); 
+	bool flush_(vector<string> match); 
+	bool straight_(vector<string> match); 
+	bool fh_(vector<string> match); 
+	int get_straight_p(char s); 
+	bool sf_(vector<string> match);
+	bool foak_(vector<string> match); 
+	bool rf_(vector<string> match); 
+	
 	
 	
 };
+
+bool poker:: rf_(vector<string> match) 
+{
+	bool rf=false;
+	bool flush = flush_(match); 
+	bool str8 = straight_(match);
+	
+	int sz=tstraight.size();
+	
+
+	
+	
+	int count; 
+	
+	if(flush && str8)
+	{
+		for(int i=0; i<sz; i++) 
+		{ 
+			if(tstraight[i] == 't')
+			{
+				count++; 
+			} 
+			if(tstraight[i] == 'j')
+			{
+				count++; 
+			} 
+			if(tstraight[i] == 'q')
+			{
+				count++; 
+			} 
+			if(tstraight[i] == 'k')
+			{
+				count++; 
+			}
+			if(tstraight[i] == 'a')
+			{
+				count++; 
+			} 
+			
+			//cout<<tstraight[i]<<"\n";
+		} 
+		
+	}
+	
+	if(count==5) 
+	{
+		rf=true;
+	}
+	
+	return rf; 
+	
+}
+
+bool poker:: sf_(vector<string> match) 
+{
+	bool s , f;
+	s=straight_(match); 
+	f=flush_(match); 
+	
+	if( s && f )
+	 {
+	 	return true; 
+	 }
+	 else 
+	 {
+	 	return false;
+	 }
+}
+	
+bool poker::foak_(vector<string> match) 
+{
+	int si; 
+	si=match.size();
+	
+	bool foak=false; 
+	
+	char num, type, numt, typet;
+	string test, testt; 
+	
+	int pair;
+	
+	for(int k=0; k<si;k++)
+	{
+		pair=0; 
+		
+		test = match[k]; 
+		num=test.at(0); 
+		type=test.at(1); 
+		
+		for(int l=k+1; l<si; l++) 
+		{ 
+			
+			testt=match[l]; 
+			numt=testt.at(0); 
+			typet=testt.at(1); 
+			
+			if(num==numt)
+		    {
+		    	pair++; 
+		    } 
+		    
+		    
+		    if(pair==3 && num==numt){
+		    	foak=true;
+		    	goto found; 
+		    }
+		    
+		    
+		} 
+	
+	}
+	
+	found:
+	return foak;
+	
+	
+}
+
+
+bool poker::fh_(vector<string> match) 
+{
+	bool fh = false; 
+	if(toak_(match))
+	 {
+	 	int si; 
+	    si=match.size();
+	
+	char num, type, numt, typet,tp;
+	string test, testt; 
+	
+	int pair;
+	
+	for(int k=0; k<si;k++)
+	{
+		pair=0; 
+		
+		test = match[k]; 
+		num=test.at(0); 
+		type=test.at(1); 
+		
+		for(int l=k+1; l<si; l++) 
+		{ 
+			
+			testt=match[l]; 
+			numt=testt.at(0); 
+			typet=testt.at(1); 
+			
+			if(num==numt)
+		    {
+		    	pair++; 
+		    } 
+		    
+		    
+		    if(pair==2 && num==numt){
+		    	tp=num;
+		    	goto found; 
+		    }
+		    
+		    
+		} 
+	
+	
+	    
+	 } 
+	 
+	 found: 
+	 
+	 for(int k=0; k<si;k++)
+	{
+		// J 9 3 5 6 6
+			
+		test = match[k]; 
+		num=test.at(0); 
+		type=test.at(1); 
+		
+		for(int l=k+1; l<si; l++) 
+		{ 
+			
+			testt=match[l]; 
+			numt=testt.at(0); 
+			typet=testt.at(1); 
+			
+			if(num==numt && num!=tp)
+		    {
+		    	fh=true;
+		    	goto found2;
+		    } 
+		    
+		    
+		} 
+	
+	}
+	 
+	 
+	 
+	 
+  }
+  
+  found2:
+  
+  return fh;
+} 
+
+	
+	 	
+
+bool poker::flush_(vector<string> match) 
+{
+	int si; 
+	si=match.size();
+	
+	bool flush=false; 
+	
+	char num, type, numt, typet;
+	string test, testt; 
+	
+	int pair;
+	
+	for(int k=0; k<si-1;k++)
+	{
+		pair=0; 
+		
+		test = match[k]; 
+		num=test.at(0); 
+		type=test.at(1); 
+		
+		for(int l=k+1; l<si; l++) 
+		{ 
+			
+			testt=match[l]; 
+			numt=testt.at(0); 
+			typet=testt.at(1); 
+			
+			if(type==typet)
+		    {
+		    	pair++; 
+		    } 
+		    
+		    
+		    if(pair==4 && type==typet){
+		    	flush=true;
+		    	goto found; 
+		    }
+		    
+		    
+		} 
+	
+	}
+	
+	found:
+	return flush;
+	
+} 
+
+bool poker::straight_(vector<string> match) 
+{
+	
+	int si; 
+	si=match.size();
+	
+	bool bstraight=false; 
+	
+	char num, type, numt, typet;
+	string test, testt, temp;
+	
+	int pair;
+	int sp;
+	
+	char s1, s2, s3, s4; 
+	
+	for(int k=0; k<si; k++)
+	{	
+		
+		pair=1; 
+		tstraight.clear();
+		
+		test = match[k]; 
+		num=test.at(0); 
+		type=test.at(1); 
+		
+	    sp = get_straight_p(num);
+			
+		temp=straight[sp+1]; 
+		s1 = temp.at(0); 
+			
+		temp=straight[sp+2]; 
+		s2=temp.at(0); 
+			
+		temp=straight[sp+3];
+		s3=temp.at(0); 
+			
+		temp=straight[sp+4]; 
+		s4=temp.at(0); 
+			
+		//cout<<"straight: "<<num<<" "<<s1<<" "<<s2<<" "<<s3<<" "<<s4<<"\n";
+		
+		tstraight.push_back(num); 
+		
+		for(int l=0; l<si; l++) 
+		{ 
+			
+			
+			testt=match[l]; 
+			numt=testt.at(0); 
+			typet=testt.at(1); 
+			
+			int nsize; 
+			nsize=tstraight.size(); 
+			
+			for(int i=0; i<nsize; i++) 
+			{
+				
+				if(numt==tstraight[i])
+				{
+					goto skp;
+				} 
+			}
+			
+		
+			if(numt==s1) 
+			{
+				pair++; 
+				tstraight.push_back(s1);
+				
+			} 
+			if(numt==s2) 
+			{
+				pair++; 
+				tstraight.push_back(s2);
+			} 
+			if(numt==s3)
+			{
+				pair++; 
+				tstraight.push_back(s3); 
+			} 
+			if(numt==s4)
+			{
+				pair++;
+				tstraight.push_back(s4);
+			} 
+			
+			skp:
+			
+			if(pair==5)
+			{
+				bstraight = true;
+				goto found;
+			
+			}
+			
+			
+			
+	       }
+   }
+   
+   found:
+   return bstraight;
+   
+ } 
+   
+
+int poker:: get_straight_p(char s)
+{
+	int i;
+	int siz= straight.size();
+	string temps;
+	char temp; 
+	
+	for(i=0; i<siz; i++)
+	{
+		temps=straight[i]; 
+		
+		temp = temps.at(0); 
+		
+		if(s==temp)
+		{
+			
+			return i; 
+		} 
+	}
+	
+	
+}
+	   
 
 bool poker::toak_(vector<string> match) 
 {
@@ -55,7 +483,7 @@ bool poker::toak_(vector<string> match)
 	
 	int pair;
 	
-	for(int k=0; k<si-1;k++)
+	for(int k=0; k<si;k++)
 	{
 		pair=0; 
 		
@@ -103,7 +531,7 @@ bool poker::twopair_(vector<string> match)
 	
 	int thatsapair; 
 	
-	for(int k=0; k<si-1;k++)
+	for(int k=0; k<si;k++)
 	{
 		// J 3 3 5 6 6
 			
@@ -150,7 +578,7 @@ bool poker::pair_(vector<string> match)
 	string test, testt; 
 	
 	
-	for(int k=0; k<si-1;k++)
+	for(int k=0; k<si;k++)
 	{
 		// J 9 3 5 6 6
 			
@@ -203,10 +631,39 @@ void poker::show_equity()
 	
 	//find # matches within match<> 
 	
+	//match=testv; 
 	
+	if(rf_(match))
+	{
+		cout<<"Royal Flush";
+	}
 	
+	else if(sf_(match))
+	{
+		cout<<"Straight Flush"; 
+	}
 	
-	if(toak_(match))
+	else if(foak_(match))
+	{
+		cout<<"Four of A Kind";
+	}
+	
+	else if(fh_(match)) 
+	{
+		cout<<"Full House";
+	}
+	
+	else if(flush_(match))
+	{
+		cout<<"Flush"; 
+	}
+	
+	else if(straight_(match))
+	{
+		cout<<"Straight";
+	}
+	
+	else if(toak_(match))
 	{
 		cout<<"Three of a kind";
 	}
@@ -222,6 +679,7 @@ void poker::show_equity()
 		cout<<"High Card";
 	} 
 	
+
 	
 	cout<<"\n\n"; 
 	
@@ -440,12 +898,13 @@ void poker::reset_deck(){
 	
 	std::string v;
 	
-	for(int i=2; i<=10; i++)
+	for(int i=2; i<=9; i++)
 	{
 		v=std::to_string(i);
 		value.push_back(v);
 	}
 	
+	value.push_back("t");
 	value.push_back("j");
 	value.push_back("q");
 	value.push_back("k");
